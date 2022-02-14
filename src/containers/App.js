c2dee78c83ca158d388619da351ea7b99a0568b3
안예진 <ahw24033@naver.com>
2022-02-14 16:43:10 +0900

    components 구성_1

---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import Navigation from "components/Navigation";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <components>
+          <Navigation />
+        </components>
+      </div>
+    );
+  }
+}
+
+export default App;
