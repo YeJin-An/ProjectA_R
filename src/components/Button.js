73a8b198a74d11209cd471c9e924b1bed3170936
안예진 <ahw24033@naver.com>
2022-02-15 15:15:42 +0900

    style 지정

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ export default function Button() {
       <button
         className="bg-blue-600 hover:bg-blue-700 
         text-white 
-        font-bold py-1 px-3 rounded"
+        font-bold py-0.5 px-3 rounded mt-3 my-3"
       >
         Button
       </button>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div classNAme="my-5 p-2 text-gray-700">
+    <div>
       <hr />
       &copy; 2022. GoldKiller 컴퍼니.
     </div>
